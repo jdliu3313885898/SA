2ac5186c89d3359c75113721b347d073b184af77
jdliu <jide.liu@avepoint.com>
2017-12-22 15:54:10 +0800

    no message

--- TestGitHub/Controllers/HomeController.cs
+++ TestGitHub/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace TestGitHub.Controllers
             int i = 0;
             i = i + 1;
             i = i + 2;
-            i = i + 3;
+            i = i + 5;
             i = i + 4;
             return View();
         }
